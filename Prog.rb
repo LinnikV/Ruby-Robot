56b8dfcb1a986bcf4b1ab798e5c1e482211a7cf9
LinnikV <linnikvitek@ukr.net>
2022-02-16 16:08:40 +0200

    wtf

--- Prog.rb
+++ Prog.rb
@@ -1,85 +1,68 @@
-class Robot
-  attr_accessor :x, :y, :f
+$x = 0
+$y = 0
+$f = 0
 
-  def initialize(x, y, f)
-    @x = x
-    @y = y
-    @f = f
+  def check?(x, y)
+    case x
+      when (0..4)
+      else return false
+    end
 
+    case y
+      when (0..5)
+      else return false
+    end
+    return true
   end
 
-
-def check?(x, y)
-  case x
-  when (0..4)
-  else return false
+  def place(x, y, f)
+    if check?(x,y)
+      $x = x
+      $y = y
+      $f = f
+    end
   end
 
-  case y
-  when (0..5)
-  else return false
-  end
-  return true
-end
-
-def place(x, y, f)
-  if check?(x,y)
-  $x = x
-  $y = y
-  $f = f
-  end
-end
-
-def move
-  if check?($x, $y)
-    x = $x
-    y = $y
-  case $f
-  when :north then y = $y + 1
-  when :east then x = $x + 1
-  when :south then y = $y - 1
-  when :west then x = $x - 1
-  end
+  def move
+    if check?($x, $y)
+      x = $x
+      y = $y
+      case $f
+        when :north then y = $y + 1
+        when :east then x = $x + 1
+        when :south then y = $y - 1
+        when :west then x = $x - 1
+      end
+    end
+    if check?(x,y)
+      $x = x
+      $y = y
+    end
   end
 
-  if check?(x,y)
-    $x = x
-    $y = y
+  def right
+    if check?($x ,$y)
+      case $f
+        when :north then $f = :east
+        when :east then $f = :south
+        when :south then $f = :west
+        when :west then $f = :north
+      end
+    end
   end
-end
 
-def right
-  if check?($x ,$y)
-  case $f
-  when :north then $f = :east
-  when :east then $f = :south
-  when :south then $f = :west
-  when :west then $f = :north
+  def left
+    3.times {right}
   end
+
+  def report
+    print "#{$x},#{$y},#{$f.upcase}"
+    puts
   end
-end
 
-def left
-  if check?($x,$y)
-  case $f
-  when :north then $f = :west
-  when :west then $f = :south
-  when :south then $f = :east
-  when :east then $f = :north
-  end
-  end
-end
-
-def report
-  print $x, $y, $f
-  puts
-end
-
-
-def print_res
-
-end
-
-
-
-end
+place(1, 2, :east)
+move
+move
+left
+move
+report
